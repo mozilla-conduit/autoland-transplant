8975342ac2f956d0fdf32ad24715c45d61ecfa9a
Byron Jones <byron@glob.com.au>
2018-05-23 12:03:36 +0800

    add 'elapsed time' output after running tests

--- testing/harness/helpers.sh
+++ testing/harness/helpers.sh
@@ -1,6 +1,6 @@
 alias autolandctl="${TESTDIR}/testing/harness/autolandctl.py"
 
 setup_test_env() {
-    ${TESTDIR}/run-tests restart-docker --restart
+    ${TESTDIR}/run-tests --restart --no-timing
     hg clone ${HGWEB_URL}/test-repo client > /dev/null
 }
